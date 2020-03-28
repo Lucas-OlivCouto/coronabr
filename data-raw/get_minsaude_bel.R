7e61f713a8401830894ad71e4465207229f509fc
Andrea Sánchez Tapia <katori@gmail.com>
2020-03-27 21:50:30 -0700

    formats minsaude old data but this should not change anymore

--- data-raw/get_minsaude_bel.R
+++ data-raw/get_minsaude_bel.R
@@ -1,6 +1,14 @@
 # Faz download dos dados do Minsaúde compilados por Adriano Belisario
-
-download.file(
-  "https://raw.githubusercontent.com/belisards/coronabr/master/dados/corona_brasil.csv",
-  destfile = "data-raw/minsaude_bel.csv")
-minsaude_bel <- read.csv("data-raw/minsaude_bel.csv")
+# i checked that the info be the same as the file that we had til 18-03
+# this shouldnt be downloaded anymore because the file has changed
+#download.file(
+#  "https://raw.githubusercontent.com/belisards/coronabr/master/dados/corona_brasil.csv",
+#  destfile = "data-raw/minsaude_bel.csv")
+estados <- read.csv("data-raw/estados_code.csv", row.names = 1)
+minsaude <- read.csv("data-raw/minsaude_bel.csv") %>%
+  rename(geocode = uid) %>%
+  arrange(date) %>%
+  left_join(estados) %>%
+  select(-uf) %>%
+  filter(date != "2020-03-26")
+write.csv(minsaude, "data-raw/minsaude_formatted.csv")
