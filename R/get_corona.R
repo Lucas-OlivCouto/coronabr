e856dbd9ee6e6eb28794e5495a61ccf36ec88613
Andrea Sánchez Tapia <katori@gmail.com>
2020-03-28 20:51:05 -0700

    os dados de minsaude desde dia 26, o plot está funcionando, o get também. ainda não resolvi o workflow, para evitar um double append botei distinct() mas estou duvidando se esse novo deve ser salvo também

--- R/get_corona.R
+++ R/get_corona.R
@@ -1,6 +1,7 @@
-#' Extrai dados de corona vírus para o Brasil
+#' Extrai dados de coronavírus para o Brasil
 #'
-#' Esta função extrai os valores compilados pelo ministério da Saúde do Brasil (disponível em: 'http://plataforma.saude.gov.br/novocoronavirus/#COVID-19-brazil') e salva o resultado no disco.
+#' Esta função extrai os valores compilados pelo ministério da Saúde do Brasil
+#' (disponível em: 'https://covid.saude.gov.br/') e salva o resultado no disco.
 #'
 #' @param dir Diretório onde salvar o arquivo
 #' @param filename Nome do arquivo
@@ -11,6 +12,7 @@
 #' @importFrom utils write.csv
 #' @importFrom dplyr mutate_if
 #' @importFrom dplyr bind_rows
+#' @importFrom dplyr left_join
 #' @importFrom rlang .data
 #' @import magrittr
 #' @import plyr
@@ -18,43 +20,36 @@
 #' @export
 #'
 get_corona <- function(dir = "output/",
-                       filename = "corona_brasil"){
+                       filename = "corona_minsaude") {
   rlang::.data #para usar vars no dplyr
-  url <- 'http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js'
-  # url
-  message("Extraindo os dados ...")
-  tryCatch(
-    dados <- xml2::read_html(url) %>%
-    rvest::html_text() %>%
-    # gambiarra porque dava erro para ler o json direto
-    url %>%
-    gsub("var database=", "", .) %>%
-    jsonlite::fromJSON(encoding = "UTF-8"),
-    error = function(x) {}
-    )
-   if ( !exists("dados") ) {
-     stop("Falha no download dos dados :( Site do Min. da saude fora do ar!")
-  }
-  # AQUI PRECISA ADICIONAR MSG DE ERRO SE O OBJETO DADOS NAO É CRIADO
-  # extraindo so braSil
-  br <- dados$brazil
-  br$id_date <- 1:nrow(br)
-  vals <- br$values %>%
-    lapply(., dplyr::mutate_if, is.integer, as.character) %>%
-    dplyr::bind_rows(., .id = "id_date")
-  df <- merge(br[, c('date', 'id_date')],
-              vals[, !names(vals) %in% c('comments', 'broadcast')],
-              by = "id_date")
-  df$date <-  as.Date(df$date, format = "%d/%m/%Y")
-  new_df <- df %>%
-    dplyr::group_by(.data$id_date, .data$date, .data$uid) %>%
-    dplyr::mutate_at(dplyr::vars(-dplyr::group_cols()), as.numeric) %>%
-    dplyr::summarize_at(dplyr::vars(-dplyr::group_cols()), dplyr::funs(sum(., na.rm = TRUE)))
-  message(paste0("salvando ", filename, ".csv em ", dir))
-  if (!dir.exists(dir)) {
-    dir.create(dir)
-  }
+  #get original data and format it
+  estados <- read.csv("./data-raw/estados_code.csv", row.names = 1)
+  minsaude <-
+    read.csv("./data-raw/minsaude_formatted.csv", row.names = 1) %>%
+    dplyr::left_join(estados) %>%
+    dplyr::mutate(date = as.Date(date))
+
+  covid_br <- get_corona_minsaude()
+  #formatting only estados
+  covid_estados <- covid_br$PortalMapa
+  #estados deveria ser data. nao tem uma pasta com dados, aliñas
+  #format estados new
+  covid_estados_new <-
+    dplyr::left_join(covid_estados, estados) %>%
+    dplyr::rename(cases = qtd_confirmado) %>%
+    dplyr::select(nome, codigo, geocode, updatedAt, cases) %>%
+    dplyr::mutate(date =  as.Date(updatedAt)) %>%
+    dplyr::select(-updatedAt)
+
+  new_df <-
+    dplyr::bind_rows(minsaude, covid_estados_new) %>%
+    distinct(nome, cases, )
+
+   message(paste0("salvando ", filename, ".csv em ", dir))
+   if (!dir.exists(dir)) {
+     dir.create(dir)
+   }
   utils::write.csv(new_df, paste0(dir, filename, ".csv"),
-                   row.names = FALSE)
+                    row.names = FALSE)
   return(new_df)
 }
---
+++ data-raw/get_minsaude_new.R
@@ -0,0 +1,25 @@
+
+get_corona_minsaude <- function(write = FALSE) {
+  url <- "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/"
+  endpoints <- c("PortalGeral",
+                 "PortalRegiao",
+                 "PortalSemana",
+                 "PortalDias",
+                 "PortalAcumulo",
+                 "PortalMapa")
+  links <- paste0(url, endpoints)
+  headers <- httr::add_headers("X-Parse-Application-Id" = "unAFkcaNDeXajurGB7LChj8SgQYS2ptm")
+  covid_br <- links %>%
+    purrr::set_names(endpoints) %>%
+    purrr::map(httr::GET, headers) %>%
+    purrr::map(httr::content, simplifyDataFrame = TRUE) %>%
+    purrr::map("results") %>%
+    purrr::map(tibble::as_tibble)
+  if (write == TRUE) {
+    save(covid_br, file = paste0("./data-raw/", Sys.Date(), "_covid_br.rda"),
+         compress = "xz")
+  }
+  return(covid_br)
+}
+get_corona_minsaude(write = TRUE)
+#this should be run everyday (travis?) and will save an .rda with the whole data for the day. maybe this is unnecessary but maybe data will change.
